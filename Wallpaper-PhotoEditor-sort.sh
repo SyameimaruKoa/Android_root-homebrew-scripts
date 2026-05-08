0235ed463d7ee7f198d2746dab5307f5c05a67d9
SyameimaruKoa-S21U <toho.komeizi.aya@gmail.com>
2026-05-09 08:09:53 +0900

    イラスト整理の簡略化用ショートカットを作成 READMEは未更新

---
+++ Wallpaper-PhotoEditor-sort.sh
@@ -0,0 +1,4 @@
+#/bin/bash
+cd ~/storage/shared/イラスト編集用/Photo\ Editor
+bash ../Script/Wallpaper-AutoSort.sh .
+exit
---
+++ Wallpaper-twitter-sort.sh
@@ -0,0 +1,4 @@
+#/bin/bash
+cd ~/storage/shared/イラスト編集用/未整理/Twitter
+bash ../Script/rename-twitter.sh 
+exit.
